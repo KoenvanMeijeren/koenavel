3839498aa89957e67f287eaca79dfbd7cc827c03
Koen van Meijeren <kmeijere0303@student.landstede.nl>
2019-10-01 17:15:22 +0200

    improve view tests

--- app/services/core/ProductionErrorView.php
+++ app/services/core/ProductionErrorView.php
@@ -10,13 +10,17 @@ use Whoops\Handler\Handler;
 class ProductionErrorView extends Handler
 {
     /**
+     * Show the error page when the app is in production mode
+     *
+     * @param string $viewName The name of the production error view
+     *
      * @return int A handler may return nothing, or a Handler::HANDLE_* constant
      *
      * @throws Exception
      */
-    public function handle(): int
+    public function handle(string $viewName = '500-error'): int
     {
-        new View('500-error');
+        new View($viewName);
 
         return Handler::QUIT;
     }
