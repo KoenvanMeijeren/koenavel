4059b7dd3a05cc36789086b7cc59a5af81694e70
Koen van Meijeren <kmeijere0303@student.landstede.nl>
2019-10-22 11:46:39 +0200

    add tests for the added request methods

--- tests/unit/core/RequestTest.php
+++ tests/unit/core/RequestTest.php
@@ -18,6 +18,10 @@ class RequestTest extends TestCase
         $_POST['array'] = ['test' => 1];
         $_GET['test'] = 'test';
         $_GET['array'] = ['test' => 1];
+        $_ENV['test'] = 'test';
+        $_ENV['array'] = ['test' => 1];
+        $_COOKIE['test'] = 'test';
+        $_COOKIE['array'] = ['test' => 1];
         $_FILES['test'] = ['test' => 'test'];
 
         $this->request = new Request();
@@ -46,6 +50,32 @@ class RequestTest extends TestCase
     {
         $this->assertEquals(
             '', $this->request->get('non_existing_item')
+        );
+    }
+
+    public function test_that_we_can_get_a_env_item()
+    {
+        $this->assertEquals('test', $this->request->env('test'));
+        $this->assertIsArray(json_decode($this->request->env('array')));
+    }
+
+    public function test_that_we_cannot_get_a_env_item()
+    {
+        $this->assertEquals(
+            '', $this->request->env('non_existing_item')
+        );
+    }
+
+    public function test_that_we_can_get_a_cookie_item()
+    {
+        $this->assertEquals('test', $this->request->cookie('test'));
+        $this->assertIsArray(json_decode($this->request->cookie('array')));
+    }
+
+    public function test_that_we_cannot_get_a_cookie_item()
+    {
+        $this->assertEquals(
+            '', $this->request->cookie('non_existing_item')
         );
     }
 
