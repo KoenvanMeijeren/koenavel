34a422188084da9fb983b8174bcbe26f989eed4a
Koen van Meijeren <kmeijere0303@student.landstede.nl>
2019-11-05 14:52:31 +0100

    finish the implementation of a few todo items

--- index.php
+++ index.php
@@ -12,8 +12,5 @@ $app->run();
 
 // todo rewrite sensitive config data into .env files
 // todo add random generated session id
-// todo try to rewrite all the static methods into non-static
 // todo make local settings work
-// todo find a way to test classes which are sending headers
-// todo: Actually flash data into the session ->
-// todo store it and after using it, destroy the data
+// todo: Actually flash data into the session -> todo store it and after using it, destroy the data
