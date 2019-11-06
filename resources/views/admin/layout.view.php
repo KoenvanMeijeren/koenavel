e98fb54e1a42b9578e46600abd5b02f729e05a11
Koen van Meijeren <kmeijere0303@student.landstede.nl>
2019-11-06 11:20:07 +0100

    add fav icons

--- resources/views/admin/layout.view.php
+++ resources/views/admin/layout.view.php
@@ -27,7 +27,41 @@ $user = new \App\Models\User();
         <link rel="stylesheet" href="/resources/assets/admin/css/login.css">
     <?php endif; ?>
 
+        <!-- Title -->
         <title><?= $data['title'] ?? 'Undefined' ?></title>
+
+        <!-- Fav icon -->
+        <link rel="apple-touch-icon" sizes="57x57"
+              href="/resources/assets/admin/images/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60"
+              href="/resources/assets/admin/images/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72"
+              href="/resources/assets/admin/images/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76"
+              href="/resources/assets/admin/images/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114"
+              href="/resources/assets/admin/images/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120"
+              href="/resources/assets/admin/images/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144"
+              href="/resources/assets/admin/images/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152"
+              href="/resources/assets/admin/images/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180"
+              href="/resources/assets/admin/images/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"
+              href="/resources/assets/admin/images/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32"
+              href="/resources/assets/admin/images/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96"
+              href="/resources/assets/admin/images/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16"
+              href="/resources/assets/admin/images/favicon-16x16.png">
+        <link rel="manifest" href="/resources/assets/admin/images/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage"
+              content="/resources/assets/admin/images/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
     </head>
     <body>
         <div class="container-fluid">
