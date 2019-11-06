987cbbe0227875122c35520a155077ba35948c92
Koen van Meijeren <kmeijere0303@student.landstede.nl>
2019-11-06 15:28:54 +0100

    add dutch language to the datatables and make it more dynamically when using it

--- resources/assets/admin/js/datatables.js
+++ resources/assets/admin/js/datatables.js
@@ -1,10 +1,39 @@
-$(document).ready( function () {
-    $('#accounts').DataTable();
-} );
+$(document).ready(function () {
+    $('.table').DataTable({
+        "aaSorting": [],
+        'columnDefs': [{
+            'targets': [-1], /* column index */
+            'orderable': false, /* true or false */
+        }],
+        "language": {
+            "lengthMenu": "Toon _MENU_ gegevens per pagina",
+            "zeroRecords": "Geen gegevens gevonden",
+            "info": "Toon _PAGE_ van _PAGES_",
+            "infoEmpty": "Geen gegevens beschikbaar",
+            "infoFiltered": "(gefilterd uit _MAX_ gegevens)",
+            "decimal": ",",
+            "thousands": ".",
+            "search": "Zoeken:",
+            "emptyTable": "Geen gegevens beschikbaar in deze tabel",
+            "infoPostFix": "",
+            "loadingRecords": "Laden...",
+            "processing": "Verwerken...",
+            "paginate": {
+                "first": "Eerste",
+                "last": "Laatste",
+                "next": "Volgende",
+                "previous": "Vorige"
+            },
+            "aria": {
+                "sortAscending": ": activeren om de kolom oplopend te sorteren",
+                "sortDescending": ": activeren om de kolom aflopend te sorteren"
+            }
+        }
+    });
+});
 
-$('#accounts').DataTable( {
-    scrollY: false,
-    select: true
+$(document).ready( function () {
+    $('.table').DataTable();
 } );
 
 
