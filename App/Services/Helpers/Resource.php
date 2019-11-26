d5adc9bc4e11391fb9d0e6054b05e1b5708514fd
Koen van Meijeren <kmeijere0303@student.landstede.nl>
2019-11-26 14:02:02 +0100

    add edit table column method the resource class and make it non abstract

--- App/Services/Helpers/Resource.php
+++ App/Services/Helpers/Resource.php
@@ -4,26 +4,40 @@ declare(strict_types=1);
 
 namespace App\Services\Helpers;
 
-use App\Src\File\File;
+use App\Src\Translation\Translation;
 
-abstract class Resource
+final class Resource
 {
-    final public static function loadFlashMessage(): void
+    public static function loadFlashMessage(): void
     {
         includeFile(RESOURCES_PATH . '/views/partials/flash.view.php');
     }
 
-    final public static function loadCollapsibleDiv(
-        string $target,
-        string $form
+    public static function addTableEditColumn(
+        string $editAction,
+        string $destroyAction,
+        string $destroyMessageWarning
     ): string {
-        $id = $target;
+        return '<div class="table-edit-row flex">
+                    <a href="'.$editAction.'"
+                       class="btn btn-success flex-child"
+                       data-toggle="tooltip"
+                       data-placement="top"
+                       title="'.Translation::get('table_row_edit').'">
+                        <i class="fas fa-user-edit"></i>
+                    </a>
 
-        $file = new File(
-            RESOURCES_PATH . '/views/partials/forms/',
-            "{$form}.view.php"
-        );
-
-        return $file->get(compact('id'));
+                    <form method="post" 
+                          action="'.$destroyAction.'">
+                        <button class="btn btn-danger flex-child" 
+                                type="submit" 
+                                data-toggle="tooltip" 
+                                data-placement="top"
+                                title="'.Translation::get('table_row_delete').'" 
+                                onclick="return confirm(\''.$destroyMessageWarning.'\')">
+                            <i class="fas fa-trash-alt"></i>
+                        </button>
+                    </form>     
+                </div>';
     }
 }
