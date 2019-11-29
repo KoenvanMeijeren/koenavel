f1b91ecc97f4bb457569501d78feddb486466afd
Koen van Meijeren <kmeijere0303@student.landstede.nl>
2019-11-29 09:50:47 +0100

    add php doc

--- App/Src/Validate/Validator.php
+++ App/Src/Validate/Validator.php
@@ -7,16 +7,14 @@ namespace App\Src\Validate;
 
 class Validator
 {
-    /**
-     * @var array
-     */
-    private $input;
+    private array $input;
 
+    /**
+     * @param string[] $input
+     */
     public function __construct(array $input)
     {
         $this->input = $input;
-
-        dd($input);
     }
 
     public function required(): bool
