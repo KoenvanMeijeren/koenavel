576c79a4a46c6e1c1e51aea22412580f3e792d1a
Koen van Meijeren <kmeijere0303@student.landstede.nl>
2019-12-02 10:47:56 +0100

    remove test code

--- App/Controllers/PageController.php
+++ App/Controllers/PageController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-use App\Models\Test;
 use App\Src\Translation\Translation;
 use App\Src\View\View;
 
@@ -22,22 +21,5 @@ final class PageController
         $title = Translation::get('page_not_found_title');
 
         return new View('http/404', compact('title'));
-    }
-
-    public function test()
-    {
-        $user = new Test();
-
-        $user->updateOrCreate(
-            5,
-            [
-            'test_name' => 'Koen van Meijeren',
-            'test_city' => 'Harderwijk',
-            ]
-        );
-
-        dd(
-            $user,
-        );
     }
 }
