e10720769074c2bb29296c0136e251779f796f97
Koen van Meijeren <kmeijere0303@student.landstede.nl>
2019-12-05 14:30:27 +0100

    clean constants up

--- config/const.php
+++ config/const.php
@@ -2,10 +2,6 @@
 
 declare(strict_types=1);
 
-// Base path
-define('BASE_PATH', str_replace('\\', '/', __DIR__));
-
-// Default paths
 define('START_PATH', realpath(__DIR__.'/../'));
 define('APP_PATH', realpath(__DIR__ . '/../App'));
 define('CONFIG_PATH', realpath(__DIR__.'/../config'));
@@ -14,6 +10,3 @@ define('ROUTES_PATH', realpath(__DIR__.'/../routes'));
 define('STORAGE_PATH', realpath(__DIR__.'/../storage'));
 define('TEST_PATH', realpath(__DIR__.'/../tests'));
 define('VENDOR_PATH', realpath(__DIR__.'/../vendor'));
-
-// Asset paths
-define('ASSET_PATH', '');
