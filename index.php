053cd7462d6885c46845eb5cc2243877c00c4385
Koen van Meijeren <kmeijere0303@student.landstede.nl>
2019-12-09 10:48:19 +0100

    autoload functions and constants from composer.json

--- index.php
+++ index.php
@@ -3,9 +3,7 @@ declare(strict_types=1);
 
 use App\Src\Core\App;
 
-require_once __DIR__ . '/config/const.php';
-require_once APP_PATH . '/Functions/autoload.php';
-require_once VENDOR_PATH . '/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 $app = new App();
 $app->run();
