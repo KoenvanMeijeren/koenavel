733aaab895b4f95060ce47a871dd213efd104e1f
Koen van Meijeren <kmeijere0303@student.landstede.nl>
2019-12-09 10:51:38 +0100

    simplify auto loading

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,8 +3,6 @@ declare(strict_types=1);
 
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../config/const.php';
-require_once __DIR__ . '/../App/Functions/autoload.php';
 
 error_reporting(E_ALL | E_STRICT);
 
