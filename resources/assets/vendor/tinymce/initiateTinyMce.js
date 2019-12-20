569dc9d583df61ceaaf367d07a1a2accb9e75289
Koen van Meijeren <kmeijere0303@student.landstede.nl>
2019-12-20 13:07:46 +0100

    add features to tiny mce

--- resources/assets/vendor/tinymce/initiateTinyMce.js
+++ resources/assets/vendor/tinymce/initiateTinyMce.js
@@ -1,19 +1,20 @@
 tinymce.init({
     selector: 'textarea#content',
     height: 500,
-    plugins: [
-        "advlist autolink link image lists charmap print preview hr anchor pagebreak",
-        "searchreplace wordcount visualblocks visualchars insertdatetime media nonbreaking",
-        "table directionality emoticons paste code"
-    ],
-    menubar: "file | edit | view | format | tools",
-    toolbar1: "undo redo | formats bold italic underline | alignleft aligncenter alignright alignjustify | table bullist numlist outdent indent | styleselect",
-    toolbar2: "responsivefilemanager | link unlink anchor | image media | forecolor backcolor | preview code ",
+    plugins: 'print preview fullpage paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
+    menubar: 'file edit view insert format tools table help',
+    toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+    toolbar_sticky: true,
     content_css: [
         '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
         '//www.tiny.cloud/css/codepen.min.css'
     ],
-    convert_fonts_to_spans : false,
+    importcss_append: true,
+    quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+    noneditable_noneditable_class: "mceNonEditable",
+    toolbar_drawer: 'sliding',
+    contextmenu: "link image imagetools table",
+    convert_fonts_to_spans: false,
     browser_spellcheck: true,
     relative_urls: false,
     language: 'nl',
@@ -35,7 +36,7 @@ tinymce.init({
         xhr.withCredentials = false;
         xhr.open('POST', '/admin/upload/file');
 
-        xhr.onload = function() {
+        xhr.onload = function () {
             var json;
 
             if (xhr.status !== 200) {
