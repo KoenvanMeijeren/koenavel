e7e73fd328c16df6782da7acdd1d43afd52d7ab1
Koen van Meijeren <kmeijere0303@student.landstede.nl>
2019-12-24 15:18:21 +0100

    remove unnecessary comments

--- App/Src/Translation/Loader.php
+++ App/Src/Translation/Loader.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Src\Translation;
 
-use App\Src\Exceptions\Basic\InvalidKeyException;
 use App\Src\Exceptions\Basic\NoTranslationsForGivenLanguageID;
 
 abstract class Loader
@@ -21,29 +20,10 @@ abstract class Loader
 
     protected int $language = 0;
 
-    /**
-     * Construct the language.
-     */
     abstract protected function __construct();
 
-    /**
-     * Get a specific stored config item.
-     *
-     * @param string $key the key to search for the
-     *                    corresponding value in the translations
-     *
-     * @return string
-     * @throws InvalidKeyException
-     * @throws NoTranslationsForGivenLanguageID
-     */
     abstract public static function get(string $key): string;
 
-    /**
-     * Load the translations based on the language id.
-     *
-     * @return string[]
-     * @throws NoTranslationsForGivenLanguageID
-     */
     final protected function loadTranslations(): array
     {
         if (self::DUTCH_LANGUAGE_ID === $this->language) {
--- App/Src/Translation/Translation.php
+++ App/Src/Translation/Translation.php
@@ -6,17 +6,11 @@ namespace App\Src\Translation;
 
 use App\Src\Core\URI;
 use App\Src\Exceptions\Basic\InvalidKeyException;
-use App\Src\Exceptions\Basic\NoTranslationsForGivenLanguageID;
 
 final class Translation extends Loader
 {
     private static array $translations = [];
 
-    /**
-     * Construct the translations.
-     *
-     * @throws NoTranslationsForGivenLanguageID
-     */
     protected function __construct()
     {
         if (strpos(URI::getDomainExtension(), 'localhost') !== false
@@ -30,16 +24,6 @@ final class Translation extends Loader
         self::$translations = $this->loadTranslations();
     }
 
-    /**
-     * Get a specific stored config item.
-     *
-     * @param string $key the key to search for the
-     *                    corresponding value in the translations
-     *
-     * @return string
-     * @throws InvalidKeyException
-     * @throws NoTranslationsForGivenLanguageID
-     */
     public static function get(string $key): string
     {
         new self();
